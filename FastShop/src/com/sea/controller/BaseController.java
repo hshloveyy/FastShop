cc1fb3cb77998cc0066cd8dbb22454e1bbf375b5
hshloveyy <hshloveyy@qq.com>
2016-10-14 00:10:31 +0800

    首页设置

--- FastShop/src/com/sea/controller/BaseController.java
+++ FastShop/src/com/sea/controller/BaseController.java
@@ -2,17 +2,25 @@ package com.sea.controller;
 
 import java.util.List;
 
-import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.sea.mybatis.param.BaseParams;
 
+@RestController
 public class BaseController<T> {
 	
 	@Autowired
 	protected SqlSessionFactory sqlSessionFactory;
 
+	@RequestMapping("index")
+	public ModelAndView index(){
+		return new ModelAndView("index");
+	}
+	
 	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
 		this.sqlSessionFactory = sqlSessionFactory;
 	}
@@ -22,4 +30,5 @@ public class BaseController<T> {
 		params.setOffset(offset);
 		return sqlSessionFactory.openSession().selectList(statment, params);
 	}
+	
 }
